1bd485eb3f3835d6fe523b57bf06bf3e2dc0b9e8
Oscar Reyes <oscar.reyes@geniussportsmedia.com>
2020-03-24 21:32:42 -0500

    + Recipe recyclerview
    * Added adapter and viewholder class for Recipe
    * Adapted MainActivity to make use of RecipeAdapter for the recyclerview

--- app/src/main/java/dev/oscarreyes/bakingrecipes/MainActivity.java
+++ app/src/main/java/dev/oscarreyes/bakingrecipes/MainActivity.java
@@ -1,14 +1,60 @@
 package dev.oscarreyes.bakingrecipes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
 import android.os.Bundle;
 
+import java.util.List;
+
+import dev.oscarreyes.bakingrecipes.adapter.RecipeAdapter;
+import dev.oscarreyes.bakingrecipes.api.BakingAPI;
+import dev.oscarreyes.bakingrecipes.entity.Recipe;
+
 public class MainActivity extends AppCompatActivity {
+	private static final String TAG = MainActivity.class.getSimpleName();
+	private static final String[] PERMISSIONS = new String[]{Manifest.permission.INTERNET};
+	private static final int PERMISSION_CODE = 174;
+
+	RecyclerView recipesRecycler;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		this.loadViews();
+		this.setupLayout();
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		fetchRecipeCollection();
+	}
+
+	private void loadViews() {
+		this.recipesRecycler = this.findViewById(R.id.rv_recipes);
+	}
+
+	private void setupLayout() {
+		GridLayoutManager layoutManager = new GridLayoutManager(this, 1);
+
+		layoutManager.setOrientation(RecyclerView.VERTICAL);
+
+		this.recipesRecycler.setLayoutManager(layoutManager);
+		this.recipesRecycler.setHasFixedSize(true);
+	}
+
+	private void loadAdapter(List<Recipe> recipes) {
+		RecipeAdapter recipeAdapter = new RecipeAdapter(recipes);
+
+		this.recipesRecycler.setAdapter(recipeAdapter);
+	}
+
+	private void fetchRecipeCollection() {
+		BakingAPI.getRecipesMock(this, this::loadAdapter);
 	}
 }
---
+++ app/src/main/java/dev/oscarreyes/bakingrecipes/adapter/RecipeAdapter.java
@@ -0,0 +1,62 @@
+package dev.oscarreyes.bakingrecipes.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import dev.oscarreyes.bakingrecipes.R;
+import dev.oscarreyes.bakingrecipes.entity.Recipe;
+
+public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.RecipeViewHolder> {
+	private static final String TAG = RecipeAdapter.class.getSimpleName();
+
+	private final List<Recipe> recipes;
+
+	public RecipeAdapter(List<Recipe> recipes) {
+		this.recipes = recipes;
+	}
+
+	@NonNull
+	@Override
+	public RecipeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+		Context context = parent.getContext();
+		LayoutInflater layoutInflater = LayoutInflater.from(context);
+		View view = layoutInflater.inflate(R.layout.recipe_list_item, parent, false);
+
+		return new RecipeViewHolder(view);
+	}
+
+	@Override
+	public void onBindViewHolder(@NonNull RecipeViewHolder holder, int position) {
+		final Recipe recipe = this.recipes.get(position);
+
+		holder.bind(recipe);
+	}
+
+	@Override
+	public int getItemCount() {
+		return this.recipes.size();
+	}
+
+	class RecipeViewHolder extends RecyclerView.ViewHolder {
+		private TextView name;
+
+		public RecipeViewHolder(@NonNull View itemView) {
+			super(itemView);
+
+			this.name = itemView.findViewById(R.id.recipe_item_name);
+		}
+
+		void bind(Recipe recipe) {
+			this.name.setText(recipe.getName());
+		}
+	}
+}
+
