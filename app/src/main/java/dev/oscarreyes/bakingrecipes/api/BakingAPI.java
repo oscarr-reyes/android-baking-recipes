333d4d1e4c5a1a19ec992ff2314463e2c37dd0be
Oscar Reyes <oscar.reyes@geniussportsmedia.com>
2020-03-27 13:42:21 -0500

    + Ingredients, Steps mock fetching
    * Added function to fetch a list of ingredients by recipe index from the mock file
    * Added function to fetch a list of steps by recipe index from the mock file

--- app/src/main/java/dev/oscarreyes/bakingrecipes/api/BakingAPI.java
+++ app/src/main/java/dev/oscarreyes/bakingrecipes/api/BakingAPI.java
@@ -5,6 +5,9 @@ import android.os.AsyncTask;
 import android.util.Log;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.reflect.TypeToken;
 
 import java.io.BufferedReader;
@@ -17,13 +20,16 @@ import java.lang.reflect.Type;
 import java.util.List;
 
 import dev.oscarreyes.bakingrecipes.R;
+import dev.oscarreyes.bakingrecipes.entity.Ingredient;
 import dev.oscarreyes.bakingrecipes.entity.Recipe;
+import dev.oscarreyes.bakingrecipes.entity.Step;
 
 public class BakingAPI {
 	private static final String TAG = BakingAPI.class.getSimpleName();
 
 	/**
 	 * Reads the mock api file from raw resources
+	 *
 	 * @param context The context where to get the resource instance
 	 */
 	public static void getRecipesMock(final Context context, final RecipesResult recipesResult) {
@@ -33,7 +39,8 @@ public class BakingAPI {
 				final int resource = resources[0];
 				final Gson gson = new Gson();
 				final String jsonString = readRawResource(context, resource);
-				final Type listType = new TypeToken<List<Recipe>>(){}.getType();
+				final Type listType = new TypeToken<List<Recipe>>() {
+				}.getType();
 
 				return gson.fromJson(jsonString, listType);
 			}
@@ -46,8 +53,81 @@ public class BakingAPI {
 	}
 
 	/**
+	 * Reads the mock api file from raw resources and gets the ingredients of the selected recipe by index
+	 *
+	 * @param context           The context where to get the resource instance
+	 * @param index             The index where to extract the ingredients
+	 * @param ingredientsResult The listener for returning the result
+	 */
+	public static void getIngredientsByIndexMock(final Context context, final int index, final IngredientsResult ingredientsResult) {
+		final Gson gson = new Gson();
+
+		new AsyncTask<Integer, Void, JsonArray>() {
+			@Override
+			protected JsonArray doInBackground(Integer... resources) {
+				final int resource = resources[0];
+				final String jsonString = readRawResource(context, resource);
+				final JsonArray jsonRecipes = gson.fromJson(jsonString, JsonArray.class);
+
+				final JsonObject jsonRecipe = jsonRecipes.get(index)
+					.getAsJsonObject();
+
+				return jsonRecipe.get("ingredients")
+					.getAsJsonArray();
+			}
+
+			@Override
+			protected void onPostExecute(JsonArray jsonIngredients) {
+				final Type listType = new TypeToken<List<Ingredient>>() {
+				}.getType();
+
+				final List<Ingredient> ingredients = gson.fromJson(jsonIngredients, listType);
+
+				ingredientsResult.result(ingredients);
+			}
+		}.execute(R.raw.baking);
+	}
+
+	/**
+	 * Reads the mock api file from raw resources and gets the steps of the selected recipe by index
+	 *
+	 * @param context     The context where to get the resource instance
+	 * @param index       The index where to extract the steps
+	 * @param stepsResult The listener for returning the result
+	 */
+	public static void getStepsByIndexMock(final Context context, final int index, final StepsResult stepsResult) {
+		final Gson gson = new Gson();
+
+		new AsyncTask<Integer, Void, JsonArray>() {
+			@Override
+			protected JsonArray doInBackground(Integer... resources) {
+				final int resource = resources[0];
+				final String jsonString = readRawResource(context, resource);
+				final JsonArray jsonRecipes = gson.fromJson(jsonString, JsonArray.class);
+
+				final JsonObject jsonRecipe = jsonRecipes.get(index)
+					.getAsJsonObject();
+
+				return jsonRecipe.get("steps")
+					.getAsJsonArray();
+			}
+
+			@Override
+			protected void onPostExecute(JsonArray jsonSteps) {
+				final Type listType = new TypeToken<List<Step>>() {
+				}.getType();
+
+				final List<Step> steps = gson.fromJson(jsonSteps, listType);
+
+				stepsResult.result(steps);
+			}
+		}.execute(R.raw.baking);
+	}
+
+	/**
 	 * Reads a raw resource file String
-	 * @param context The context where to get the resource instance
+	 *
+	 * @param context  The context where to get the resource instance
 	 * @param resource The id resource of the raw file
 	 * @return The content of the file as string
 	 */
@@ -75,5 +155,13 @@ public class BakingAPI {
 
 	public interface RecipesResult {
 		void result(List<Recipe> recipes);
+	}
+
+	public interface IngredientsResult {
+		void result(List<Ingredient> ingredients);
+	}
+
+	public interface StepsResult {
+		void result(List<Step> steps);
 	}
 }
